173c756f80b83e565519615c95275fb653457887
Aldio Lisafron <lisafronaldio123@gmail.com>
2024-06-14 12:35:27 +0700

    Fix react/no-unescaped-entities

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -40,7 +40,7 @@ export default function Loader({ onLoad }: { onLoad: any }) {
             <motion.div initial={{ height: '100vh' }} exit={{ height: 0 }} transition={{ type: "spring", damping: 20 }} ref={scope} className="fixed top-0 left-0 z-[100] flex w-full justify-center items-center bg-neutral-950 overflow-hidden">
                 <div className="relative flex font-branch text-5xl md:text-8xl w-full">
                     <span className="opacity-0">&nbsp;</span>
-                    <span className="absolute inset-x-0 flex justify-center"><span id="its" className="hidden">it's</span></span>
+                    <span className="absolute inset-x-0 flex justify-center"><span id="its" className="hidden">it&apos;s</span></span>
                     <span className="absolute inset-x-0 flex justify-center"><span id="your" className="hidden">your</span></span>
                     <span className="absolute inset-x-0 flex justify-center py-2"><span id="birthday" className="hidden">birthday!</span></span>
                 </div>
